6a4e95e07314a414d46f697d5760ab4e5660dddd
Francisco Annoni <francisco.annoni@gmail.com>
2026-04-03 13:42:19 -0300

    fix(05-04): add unsplash.com to next/image remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,10 @@ const nextConfig: NextConfig = {
         hostname: "orxstqqcsxatxaprqyvq.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 };
